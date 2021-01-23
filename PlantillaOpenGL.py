f60e91c5b09ad6074bd5b003323ef627e199efd0
Emiliano Mawe Macbook <emiliano@mawetecnologias.com>
2021-01-23 14:50:48 -0700

    Platilla OpenGL

---
+++ PlantillaOpenGL.py
@@ -0,0 +1,73 @@
+from OpenGL.GL import *
+from glew_wish import *
+import glfw
+
+def dibujar():
+    #rutinas de dibujo
+    glBegin()
+    
+    glEnd()
+
+def main():
+    #inicia glfw
+    if not glfw.init():
+        return
+    
+    #crea la ventana, 
+    # independientemente del SO que usemos
+    window = glfw.create_window(800,600,"Mi ventana", None, None)
+
+    #Configuramos OpenGL
+    glfw.window_hint(glfw.SAMPLES, 4)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR,3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR,3)
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+    #Validamos que se cree la ventana
+    if not window:
+        glfw.terminate()
+        return
+    #Establecemos el contexto
+    glfw.make_context_current(window)
+
+    #Activamos la validación de 
+    # funciones modernas de OpenGL
+    glewExperimental = True
+
+    #Inicializar GLEW
+    if glewInit() != GLEW_OK:
+        print("No se pudo inicializar GLEW")
+        return
+
+    #Obtenemos versiones de OpenGL y Shaders
+    version = glGetString(GL_VERSION)
+    print(version)
+
+    version_shaders = glGetString(GL_SHADING_LANGUAGE_VERSION)
+    print(version_shaders)
+
+    while not glfw.window_should_close(window):
+        #Establece regiond e dibujo
+        glViewport(0,0,800,600)
+        #Establece color de borrado
+        glClearColor(0.4,0.8,0.1,1)
+        #Borra el contenido de la ventana
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        #Dibujar
+        dibujar()
+
+        #Preguntar si hubo entradas de perifericos
+        #(Teclado, mouse, game pad, etc.)
+        glfw.poll_events()
+        #Intercambia los buffers
+        glfw.swap_buffers(window)
+
+    #Se destruye la ventana para liberar memoria
+    glfw.destroy_window(window)
+    #Termina los procesos que inició glfw.init
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
